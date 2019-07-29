7d427f475f07f611ec9a1ef3c73349c02d019a06
Anton Zekeriev <antonzekeriev@gmail.com>
2019-07-29 22:19:00 +0200

    Lugares...

---
+++ objetos/lugares.js
@@ -0,0 +1,45 @@
+let lugares = [
+
+    {
+        id: 1,
+        tipo: "teatro",
+        nombre: "Arte & Desmayo",
+        provincia: "Madrid",
+        ciudad: "Madrid",
+        cp: "28019",
+        direccion: "Calle Baleares, 14",
+        latitud: 40.394919,
+        longitud: -3.713510,
+        url: "http://www.arteydesmayo.com/",
+        aforo: "50-100"
+    },
+
+    {
+        id: 2,
+        tipo: "teatro",
+        nombre: "Plot Point",
+        provincia: "Madrid",
+        ciudad: "Madrid",
+        cp: "28005",
+        direccion: "Calle de Ercilla, 29",
+        latitud: 40.401951,
+        longitud: -3.702099,
+        url: "https://www.plotpoint.es/",
+        aforo: "0-50",
+    },
+
+    {
+        id: 3,
+        tipo: "teatro",
+        nombre: "AZarte",
+        provincia: "Madrid",
+        ciudad: "Madrid",
+        cp: "28004",
+        direccion: "Calle de San Marcos, 19",
+        latitud: 40.421392,
+        longitud: -3.698372,
+        url: "http://azarte.com",
+        aforo: "50-100",
+    },
+
+];
